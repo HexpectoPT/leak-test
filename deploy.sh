293351d4cc46cad78e89af721a2a7044340a5284
HexpectoPT <pt1.hexpecto@gmail.com>
2025-02-18 11:06:03 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Deployment script
+echo "Starting deployment..."
+
+AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+echo "Deployment complete."
