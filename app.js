b00538de229dafac3922070f0014b29a06a41b97
HexpectoPT <pt1.hexpecto@gmail.com>
2025-02-18 11:05:45 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// Application Configuration
+
+const API_SECRET = "ghp_xYZ1abcDEF345ghiJKL678mnoPQR912stuv";
+const DB_PASSWORD = "SuperSecurePass123!";
+const config = {
+    database: "mongodb://admin:SuperSecurePass123!@localhost:27017/appdb",
+    secretKey: API_SECRET
+};
+
+console.log("Application started...");
